3edc4d9638e5dc326ee80cc7c7ef05d560b01bd7
erik h <erikh.888@gmail.com>
2018-08-27 11:37:19 +0200

    add posterior label calculations

--- emStep.R
+++ emStep.R
@@ -1,3 +1,26 @@
+#===================================================#
+# Minimal EM clustering function. Does one EM step. #
+#===================================================#
+
+# multivariate normal pdf in terms of mean and precision := covariance^-1
+mvNormPdf <- function(x, mu, precision) {
+  r <- as.numeric(x - mu)
+  dens <- exp(-0.5 * t(r) %*% precision %*% r)
+  normalization <- det(precision) / (2 * pi)^(length(x) / 2)
+  return(dens / normalization)
+}
+
+# compute the max posterior probability cluster from the summary stats
+mapCluster <- function(x, labels, priorProbs, means, precisions) {
+  nLabels <- length(labels)
+  posteriorProbs <- rep(0, nLabels)
+  for (i in seq(nLabels)) {
+    posteriorProbs[i] <- priorProbs[i] * mvNormPdf(x, means[i, ], precisions[i, , ])
+  }
+  # since we only want the maximum, normalization is not needed
+  return(labels[which.max(posteriorProbs)])
+}
+
 # given data and current labels, return new labels after single em step.
 emStep <- function(data, clusters) {
   labels <- unique(clusters)
@@ -20,7 +43,13 @@ emStep <- function(data, clusters) {
   }
 
   # compute posterior probabilities and new clusters
-  for (i in seq(nrow(data))) {
+  precs <- Sigmas # precision matrices
+  for (i in dim(precs)[1]) {
+    precs[i, , ] <- solve(precs[i, , ])
   }
-  return(list(mu = means, Sigma = Sigmas))
+  output <- rep(NaN, length(clusters))
+  for (i in seq(nrow(data))) {
+    output[i] <- mapCluster(data[i, ], labels, priorProbs, means, precs)
+  }
+  return(list(mu = means, Sigma = Sigmas, clusters = output))
 }
--- test.R
+++ test.R
@@ -1,5 +1,10 @@
+source("emStep.R")
 data("iris")
-ls()
 # random starting point
 clusters <- sample(c(1,2,3), nrow(iris), replace = TRUE)
-plot(iris[,1], iris[,2], col = clusters)
+clusters2 <- emStep(iris[,-5], clusters)$clusters
+par(mfrow = c(1,2))
+plot(iris[,1], iris[,3], col = clusters)
+title("Before clustering")
+plot(iris[,1], iris[,3], col = clusters2)
+title("After one EM step")
